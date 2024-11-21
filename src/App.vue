82982916b3891744879ef4d5de77c81087b4cd6d
Mohamed Koujil <mohamed.koujil.7e7@itb.cat>
2024-11-21 17:42:29 +0100

    watch Array

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WatchArrayExample from './components/WatchArrayExample.vue'
+</script>
+
+<template>
+  <div>
+    <WatchArrayExample />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/WatchArrayExample.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, watch } from 'vue'
+const numbers = ref([])
+
+watch(numbers, (numbers) => console.log('new number added:' + numbers), { deep: 'true' })
+
+const randomNum = () => numbers.value.push(Math.random())
+</script>
+<template>
+  <button @click="randomNum">Add Random Number</button>
+</template>
